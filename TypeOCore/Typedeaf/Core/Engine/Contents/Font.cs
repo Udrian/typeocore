4aa46bd848ff9547c3ddbaec13b68899a6f69b73
Simon 'Udrian' Haggren <simonhaggren@gmail.com>
2022-11-18 17:55:41 +0100

    Added summary commenets on Texture and Font

--- TypeOCore/Typedeaf/Core/Engine/Contents/Font.cs
+++ TypeOCore/Typedeaf/Core/Engine/Contents/Font.cs
@@ -4,12 +4,27 @@ namespace TypeOEngine.Typedeaf.Core
 {
     namespace Engine.Contents
     {
+        /// <summary>
+        /// Abstract base class for Font content
+        /// </summary>
         public abstract class Font : Content
         {
+            /// <summary>
+            /// Size of Font
+            /// </summary>
             public virtual int FontSize { get; set; }
 
+
+            /// <summary>
+            /// Do not call directly, should be loaded through ContentLoader.LoadContent
+            /// </summary>
             protected Font() { }
 
+            /// <summary>
+            /// Measures the string based on the font, font size and text applied
+            /// </summary>
+            /// <param name="text"></param>
+            /// <returns></returns>
             public abstract Vec2 MeasureString(string text);
         }
     }
--- TypeOCore/Typedeaf/Core/Engine/Contents/Texture.cs
+++ TypeOCore/Typedeaf/Core/Engine/Contents/Texture.cs
@@ -4,10 +4,19 @@ namespace TypeOEngine.Typedeaf.Core
 {
     namespace Engine.Contents
     {
+        /// <summary>
+        /// Abstract base class for Texture content
+        /// </summary>
         public abstract class Texture : Content
         {
+            /// <summary>
+            /// Pixel size of Texture
+            /// </summary>
             public Vec2 Size { get; protected set; }
 
+            /// <summary>
+            /// Do not call directly, should be loaded through ContentLoader.LoadContent
+            /// </summary>
             protected Texture() { }
         }
     }
